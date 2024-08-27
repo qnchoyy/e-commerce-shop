8bb5da395ed131ff28dbd56cb00eb44d7eeb8ff2
qnchoyy <qni191488@abv.bg>
2024-08-27 18:10:53 +0300

    add basic Header

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# e-commerce-shop
+## Initialize Project
+
+- [x] Initialize git repo
+- [x] Add base vite react project
+- [x] CleanUp
+- [x] Install react-router-dom
+- [x] Setup react-router-dom
+
+## Implementation
+
+- [x] Header
+- [ ] Home
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerLogo}>
+        <Link to="/">MyStore</Link>
+      </div>
+      <nav className={styles.headerNav}>
+        <ul className={styles.headerList}>
+          <li className={styles.navItem}>
+            <Link to="/prodcuts">Products</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link to="/login">Log in</Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link to="/cart">Cart</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
