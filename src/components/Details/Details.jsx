0a23d306558a8ef9740483bb2320f6d9b2485f67
qnchoyy <qni191488@abv.bg>
2024-08-30 20:37:19 +0300

    implement details

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import styles from "./Details.module.css";
+
+export default function Details() {
+  const { id } = useParams();
+  const [product, setProduct] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+        const data = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProduct();
+  }, [id]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.productDetailsCard}>
+        <img
+          src={product.image}
+          alt={product.title}
+          className={styles.productImage}
+        />
+        <h2 className={styles.productTitle}>{product.title}</h2>
+        <p className={styles.productDescription}>{product.description}</p>
+        <p className={styles.productPrice}>Price: ${product.price}</p>
+        <div className={styles.buttons}>
+          <button className={styles.addToCartButton}>Add to Cart</button>
+          <button className={styles.goBackButton} onClick={() => navigate(-1)}>
+            Go Back
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
