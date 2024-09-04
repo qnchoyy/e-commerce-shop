a6a2fe183615029bbc02cfa61ffbe3b86b123f50
qnchoyy <qni191488@abv.bg>
2024-09-04 20:23:53 +0300

    add the spinner whet fetching data

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -3,10 +3,12 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import styles from "./Details.module.css";
 import { CartContext } from "../context/CartContext";
+import Spinner from "../Spinner/Spinner";
 
 export default function Details() {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const { addToCart } = useContext(CartContext);
 
@@ -16,8 +18,10 @@ export default function Details() {
         const response = await fetch(`https://fakestoreapi.com/products/${id}`);
         const data = await response.json();
         setProduct(data);
+        setIsLoading(false);
       } catch (error) {
         console.error(error);
+        setIsLoading(false);
       }
     };
     fetchProduct();
@@ -25,27 +29,34 @@ export default function Details() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.productDetailsCard}>
-        <img
-          src={product.image}
-          alt={product.title}
-          className={styles.productImage}
-        />
-        <h2 className={styles.productTitle}>{product.title}</h2>
-        <p className={styles.productDescription}>{product.description}</p>
-        <p className={styles.productPrice}>Price: ${product.price}</p>
-        <div className={styles.buttons}>
-          <button
-            onClick={() => addToCart(product)}
-            className={styles.addToCartButton}
-          >
-            Add to Cart
-          </button>
-          <button className={styles.goBackButton} onClick={() => navigate(-1)}>
-            Go Back
-          </button>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className={styles.productDetailsCard}>
+          <img
+            src={product.image}
+            alt={product.title}
+            className={styles.productImage}
+          />
+          <h2 className={styles.productTitle}>{product.title}</h2>
+          <p className={styles.productDescription}>{product.description}</p>
+          <p className={styles.productPrice}>Price: ${product.price}</p>
+          <div className={styles.buttons}>
+            <button
+              onClick={() => addToCart(product)}
+              className={styles.addToCartButton}
+            >
+              Add to Cart
+            </button>
+            <button
+              className={styles.goBackButton}
+              onClick={() => navigate(-1)}
+            >
+              Go Back
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,10 +3,12 @@ import { useContext, useEffect, useState } from "react";
 import styles from "./Products.module.css";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
+import Spinner from "../Spinner/Spinner";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
 
   const { addToCart } = useContext(CartContext);
 
@@ -16,8 +18,10 @@ export default function Products() {
         const response = await fetch("https://fakestoreapi.com/products");
         const data = await response.json();
         setProducts(data);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching products:", error);
+        setIsLoading(false);
       }
     };
 
@@ -27,37 +31,43 @@ export default function Products() {
   return (
     <>
       <div className={styles.productsList}>
-        <ul className={styles.products}>
-          {products.map((product) => (
-            <li key={product.id} className={styles.productCard}>
-              <img
-                className={styles.productImage}
-                src={product.image}
-                alt={product.title}
-                width="100"
-              />
-              <button
-                className={`${styles.detailsButton} ${styles.eyeIcon}`}
-                onClick={() => navigate(`/details/${product.id}`)}
-              >
-                <i className="fa fa-eye"></i>
-              </button>
-              <button
-                className={styles.addToCartButton}
-                onClick={() => addToCart(product)}
-              >
-                <i className="fa fa-plus"></i>
-              </button>
-              <h2 className={styles.productTitle}>{product.title}</h2>
-              {/* <p className={styles.productDescription}>{product.description}</p> */}
-              <p className={styles.productPrice}>Price: ${product.price}</p>
-              <p className={styles.productRating}>
-                Rating: {product.rating.rate} ({product.rating.count} reviews)
-              </p>
-              {/* <button className={styles.addToCartButton}>Add to Cart</button> */}
-            </li>
-          ))}
-        </ul>
+        {isLoading ? (
+          <div className={styles.spinnerContainer}>
+            <Spinner />
+          </div>
+        ) : (
+          <ul className={styles.products}>
+            {products.map((product) => (
+              <li key={product.id} className={styles.productCard}>
+                <img
+                  className={styles.productImage}
+                  src={product.image}
+                  alt={product.title}
+                  width="100"
+                />
+                <button
+                  className={`${styles.detailsButton} ${styles.eyeIcon}`}
+                  onClick={() => navigate(`/details/${product.id}`)}
+                >
+                  <i className="fa fa-eye"></i>
+                </button>
+                <button
+                  className={styles.addToCartButton}
+                  onClick={() => addToCart(product)}
+                >
+                  <i className="fa fa-plus"></i>
+                </button>
+                <h2 className={styles.productTitle}>{product.title}</h2>
+                {/* <p className={styles.productDescription}>{product.description}</p> */}
+                <p className={styles.productPrice}>Price: ${product.price}</p>
+                <p className={styles.productRating}>
+                  Rating: {product.rating.rate} ({product.rating.count} reviews)
+                </p>
+                {/* <button className={styles.addToCartButton}>Add to Cart</button> */}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
